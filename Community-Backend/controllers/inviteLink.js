5d2062f90096b62f4d90ea9ed3042d1b64d8d338
AtharvaH29 <9ife24bev051@kiettech.ac.in>
2025-09-02 19:10:38 +0530

    invite fix

--- Community-Backend/controllers/inviteLink.js
+++ Community-Backend/controllers/inviteLink.js
@@ -20,9 +20,9 @@ inviteLandingRouter.get('/', async (req, res) => {
 
   const FE = config.FRONTEND_URL;
   if (req.user) {
-    return res.redirect(`${FE}/community/join/${invite.community}`);
+    return res.redirect(`${FE}/dashboard?invite=${invite.community}`);
   }
-  return res.redirect(`${FE}/login?return_to=${encodeURIComponent(`/community/join/${invite.community}`)}`);
+  return res.redirect(`${FE}/auth/login?invite=${invite.community}`);
 });
 
 module.exports = inviteLandingRouter;
